6ed2c283e93ee94e35f1d03e70b10aed14793a05
Dhamith Hewamullage <hewamullage123@gmail.com>
2018-08-30 09:26:54 +0530

    HW control structure

--- radapp.py
+++ radapp.py
@@ -2,9 +2,42 @@ import RPi.GPIO as gpio
 import time
 from camera_stream import CameraStream 
 
-
 class RADApp:
     started = False
+    currentOp = 'stopped'
+    sleepTime = 0.5
+
+    def rightMotor(self, op):
+        if op == 'forward':
+            gpio.output(7, True)
+            gpio.output(11, True)
+            gpio.output(13, True)
+            gpio.output(15, False)
+        elif op == 'stop':
+            gpio.output(7, False)
+            gpio.output(11, False)
+            gpio.output(13, False)
+        elif op == 'backward':
+            gpio.output(7, True)
+            gpio.output(11, True)
+            gpio.output(13, True)
+            gpio.output(15, False)
+    
+    def leftMotor(self, op):
+        if op == 'forward':
+            gpio.output(7, True)
+            gpio.output(11, True)
+            gpio.output(13, True)
+            gpio.output(15, False)
+        elif op == 'stop':
+            gpio.output(7, False)
+            gpio.output(11, False)
+            gpio.output(13, False)
+        elif op == 'backward':
+            gpio.output(7, True)
+            gpio.output(11, True)
+            gpio.output(13, True)
+            gpio.output(15, False)
 
     def setup(self):
         gpio.setup(7, gpio.OUT)
@@ -13,31 +46,44 @@ class RADApp:
         gpio.setup(15, gpio.OUT)
 
     def start(self):
-        print('Starting hexapod...')
         self.setup()
         self.broadcast()
+        self.currentOp = 'idle'
 
     def stop(self):
         self.cameraStream.stop()
+        self.leftMotor('stop')
+        self.rightMotor('stop')
+        gpio.cleanup()
+        self.currentOp = 'stopped'
 
     def goForward(self):
-        print('going forward...')
-        gpio.output(7, True)
-        gpio.output(11, True)
-        gpio.output(13, True)
-        gpio.output(15, False)
+        self.leftMotor('forward')
+        self.rightMotor('forward')
+        self.currentOp = 'forward'
 
     def goBackward(self):
-        print('going backward...')
+        self.leftMotor('backward')
+        self.rightMotor('backward')
+        self.currentOp = 'backward'
 
     def goLeft(self):
-        print('going left...')
+        self.leftMotor('stop')
+        time.sleep(self.sleepTime)
+        if self.currentOp == 'forward':
+            self.leftMotor('forward')
+        elif self.currentOp == 'backward':
+            self.leftMotor('backward')
 
     def goRight(self):
-        print('going right...')
+        self.rightMotor('stop')
+        time.sleep(self.sleepTime)
+        if self.currentOp == 'forward':
+            self.rightMotor('forward')
+        elif self.currentOp == 'backward':
+            self.rightMotor('backward')
 
     def broadcast(self):
-        print('broadcasting...')
         self.cameraStream = CameraStream()
         self.cameraStream.stream('127.0.0.1', 5000)
 
